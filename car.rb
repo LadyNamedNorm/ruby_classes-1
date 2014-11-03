9d9b170e7fd3bf10c766e42de533f0d7ca10ec68
Nora Neyland <noraneyland@Noras-MBP-2.attlocal.net>
2014-11-02 19:48:15 -0600

    Removed parentheses

--- car.rb
+++ car.rb
@@ -1,10 +1,10 @@
 class Car
-	def initialize()
+	def initialize
 		@fuel = 10
 		@distance = 0
 	end
 
-	def get_info()
+	def get_info
 		"I'm a car. I've driven #{@distance} miles and have #{@fuel} gallons of gas left."
 	end
 
